f4336e180a09fdfe5c5e967c013f27f7cdb0b441
Indy660 <49240947+Indy660@users.noreply.github.com>
2019-06-07 02:01:26 +0300

    Update script.js
    Версия из веб-шторма, вручную перекопировал

--- script.js
+++ script.js
@@ -1,51 +1,86 @@
 let dateMonaco = new Date(2019, 4, 26, 16, 0)
 const dateMonacoHTML = document.getElementById('dateMonaco');
-dateMonacoHTML.innerHTML = "26 May 2019"
+dateMonacoHTML.innerHTML = "26 мая 2019"
 timePrint("Monacoleft", dateMonaco)
 
 let dateCanada = new Date(2019, 5, 9, 15, 15)
 const dateCanadaHTML = document.getElementById('dateCanada');
-dateCanadaHTML.innerHTML = "9 June 2019"
+dateCanadaHTML.innerHTML = "9 июня 2019"
 timePrint("Canadaleft", dateCanada)
 
 let dateFrance = new Date(2019, 6, 23, 15, 0)
 const dateFranceHTML = document.getElementById('dateFrance');
-dateFranceHTML.innerHTML = "23 Jule 2019"
+dateFranceHTML.innerHTML = "23 июня 2019"
 timePrint("Franceleft", dateFrance)
 
 let dateToday = new Date();
 let day = dateToday.getDate();
 let month = dateToday.getMonth() + 1;
 let year = dateToday.getFullYear();
+day=checkTime(day)
+month=checkTime(month)
 const dateTodayHTML = document.getElementById('dateToday');
-dateTodayHTML.innerHTML += day + '.' + month + '.' + year
+dateTodayHTML.innerHTML +=" " + day + '.' + month + '.' + year
 
 
 function timeLeft(timeRace) {
-  let dateToday = new Date();
-  if (dateToday > timeRace) {
-    return "Гонка уже прошла"
-  } else {
-    let daysLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000 * 3600 * 24));
-    let hoursLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000 * 3600) - daysLag * 24);
-    let minitesLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000 * 60) - daysLag * 24 * 60 - hoursLag * 60);
-    let secondsLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000) - daysLag * 24 * 60 * 60 - hoursLag * 60 * 60 - minitesLag * 60);
-    hoursLag = checkTime(hoursLag);
-    minitesLag = checkTime(minitesLag);
-    secondsLag = checkTime(secondsLag);
-    return daysLag + ":" + hoursLag + ":" + minitesLag + ":" + secondsLag;
-  }
+    let dateToday = new Date();
+    if (dateToday > timeRace) {
+        return "Гонка уже прошла"
+    } else {
+        let daysLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000 * 3600 * 24));
+        let hoursLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000 * 3600) - daysLag * 24);
+        let minitesLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000 * 60) - daysLag * 24 * 60 - hoursLag * 60);
+        let secondsLag = Math.floor((timeRace.getTime() - dateToday.getTime()) / (1000) - daysLag * 24 * 60 * 60 - hoursLag * 60 * 60 - minitesLag * 60);
+        hoursLag = checkTime(hoursLag);
+        minitesLag = checkTime(minitesLag);
+        secondsLag = checkTime(secondsLag);
+        return daysLag + ":" + hoursLag + ":" + minitesLag + ":" + secondsLag;
+    }
 }
 
 function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i
-  }
-  return i;
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i;
 }
 
 function timePrint(raceleft, timeRace) {
-return setInterval(function() {
-  document.getElementById(raceleft).textContent = timeLeft(timeRace);
-}, 500);
+    return setInterval(function() {
+        document.getElementById(raceleft).textContent = timeLeft(timeRace);
+    }, 500);
 }
+
+let p = document.getElementById('calendar');
+window.setInterval(changeColors, 50);  //cмена цвета на 15 каждые полсекунды
+
+let colors = { r:255, g:0, b:0 };
+
+function changeColors() {
+    const c = colors;
+    if (     c.r == 255 && c.g != 255 && c.b == 0)   c.g += 15;
+    else if (c.r != 0   && c.g == 255 && c.b == 0)   c.r -= 15;
+    else if (c.r == 0   && c.g == 255 && c.b != 255) c.b += 15;
+    else if (c.r == 0   && c.g != 0   && c.b == 255) c.g -= 15;
+    else if (c.r != 255 && c.g == 0   && c.b == 255) c.r += 15;
+    else if (c.r == 255 && c.g == 0   && c.b != 0)   c.b -= 15;
+
+    return p.style.color = `rgb(${c.r}, ${c.g}, ${c.b})`;
+}
+
+
+const button = document.getElementById('input_button_bg_change');
+const option = document.getElementsByTagName('select')[0];
+const body = document.getElementsByTagName('body')[0];
+button.onclick = function() {
+    body.style.backgroundColor = option.value
+};
+
+const button2 = document.getElementById('epilepsia');
+button2.onclick = function() {
+    let i=0
+    setInterval(()=>body.style.backgroundColor=`hsl(${++i%360},100%,50%)`,1)
+};
+
+
